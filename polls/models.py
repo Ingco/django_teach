aeba16746c021440d01f94dbc7e3d68c8f57356e
Gubatenko Artem <ingco@mail.ru>
2018-11-23 23:38:33 +0300

    Add Choice model

--- polls/models.py
+++ polls/models.py
@@ -21,6 +21,14 @@ class Question(models.Model):
     def was_published_recently(self):
         return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
 
+    was_published_recently.admin_order_field = 'pub_date'
+
+    # Тип отображения на экране - картинка: галочка или крестик
+    # was_published_recently.boolean = True
+
+    # Текст заголовка в админке
+    was_published_recently.short_description = 'Опубликован недавно?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
